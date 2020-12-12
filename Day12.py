f8de1ee54b7b90f54bad44a23687fcae41d38471
wprice99 <6424705+wprice99@users.noreply.github.com>
2020-12-12 17:41:50 +0000

    Day 12 - Code cleanup

--- Day12.py
+++ Day12.py
@@ -1,30 +1,15 @@
 
 
 class Ferry :
-    def __init__(self) :
-        self.__x = 0
-        self.__y = 0
-        self.__direction = 0    # 0 = E, 90 = N, 180 = W, 270 = S
-        self.__directionOffset = { 0 : (1, 0), 90 : (0, 1), 180 : (-1, 0), 270 : (0, -1) }
+    def __init__(self, initial_pos = (0, 0), motionMethod = None) :
+        self.__motionMethod = motionMethod
+        self.__x = initial_pos[0]
+        self.__y = initial_pos[1]
 
     def move(self, action, value) :
-        if action == 'N' :
-            self.__y += value
-        elif action == 'S' :
-            self.__y -= value
-        elif action == 'E' :
-            self.__x += value
-        elif action == 'W' :
-            self.__x -= value
-        elif action == 'L' :
-            self.__direction = (self.__direction + value) % 360
-        elif action == 'R' :
-            self.__direction = (self.__direction - value + 360) % 360
-        elif action == 'F' :
-            self.__x += value * self.__directionOffset[self.__direction][0]
-            self.__y += value * self.__directionOffset[self.__direction][1]
-        else:
-            raise RuntimeError('Invalid action {}'.format(action))
+        offset = self.__motionMethod.move(action, value)
+        self.__x += offset[0]
+        self.__y += offset[1]
 
     @property
     def x(self) :
@@ -35,21 +20,40 @@ class Ferry :
         return self.__y
 
 
-class Waypoint :
-    def __init__(self, ferry, init_x, init_y) :
-        self.__ferry = ferry
-        self.__x = init_x
-        self.__y = init_y
+class NoWaypointMethod :
+    __moveActions = { 'N': (0, 1), 'S': (0, -1), 'E': (1, 0), 'W': (-1, 0) }
+    __dirActions = { 0: (1, 0), 90: (0, 1), 180: (-1, 0), 270: (0, -1) }
+
+    def __init__(self) :
+        self.__direction = 0
 
     def move(self, action, value) :
-        if action == 'N':
-            self.__y += value
-        elif action == 'S':
-            self.__y -= value
-        elif action == 'E':
-            self.__x += value
-        elif action == 'W':
-            self.__x -= value
+        if action in self.__moveActions :
+            return (self.__moveActions[action][0] * value, self.__moveActions[action][1] * value)
+        elif action == 'L' :
+            self.__direction = (self.__direction + value) % 360
+            return (0, 0)
+        elif action == 'R' :
+            self.__direction = (self.__direction + 360 - value) % 360
+            return (0, 0)
+        elif action == 'F' :
+            return (self.__dirActions[self.__direction][0] * value, self.__dirActions[self.__direction][1] * value)
+        else:
+            raise RuntimeError('Invalid action {}'.format(action))
+
+
+class WaypointMethod :
+    __moveActions = { 'N': (0, 1), 'S': (0, -1), 'E': (1, 0), 'W': (-1, 0) }
+
+    def __init__(self, initial) :
+        self.__x = initial[0]
+        self.__y = initial[1]
+
+    def move(self, action, value) :
+        if action in self.__moveActions :
+            self.__x += self.__moveActions[action][0] * value
+            self.__y += self.__moveActions[action][1] * value
+            return (0, 0)
         elif action == 'L':
             if value == 90 :
                 self.__x, self.__y = -self.__y, self.__x
@@ -57,11 +61,11 @@ class Waypoint :
                 self.__x, self.__y = -self.__x, -self.__y
             elif value == 270 :
                 self.__x, self.__y = self.__y, -self.__x
+            return (0, 0)
         elif action == 'R':
-            self.move('L', 360-value)
+            return self.move('L', 360-value)
         elif action == 'F' :
-            self.__ferry.move('E', value * self.__x)
-            self.__ferry.move('N', value * self.__y)
+            return (self.__x * value, self.__y * value)
 
 
 # When reading the file, surround the data with 'X' to make processing easier
@@ -75,17 +79,15 @@ def load_actions() :
 
 actions = load_actions()
 
-
-ferry = Ferry()
+ferry = Ferry(motionMethod = NoWaypointMethod())
 for action in actions :
     ferry.move(action[0], action[1])
 
-print(ferry.x, ferry.y, abs(ferry.x)+abs(ferry.y))
+print('Final Pos: {}  Distance: {}'.format((ferry.x, ferry.y), abs(ferry.x)+abs(ferry.y)))
 
 
-ferry = Ferry()
-waypoint = Waypoint(ferry, 10, 1)
+ferry = Ferry(motionMethod = WaypointMethod(initial = (10, 1)))
 for action in actions :
-    waypoint.move(action[0], action[1])
+    ferry.move(action[0], action[1])
 
-print(ferry.x, ferry.y, abs(ferry.x)+abs(ferry.y))
+print('Final Pos: {}  Distance: {}'.format((ferry.x, ferry.y), abs(ferry.x)+abs(ferry.y)))
